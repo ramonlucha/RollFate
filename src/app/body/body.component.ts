12f8e8aea54a1721e59af2fd0842c10b1d5938d4
Ramón Lucha <ramonlucha@gmail.com>
2018-05-24 02:10:01 +0200

    logic and web changes commit

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
 
-  constructor() { }
+  public dices: number[]=[3,3,3,3];
+  
+    constructor() {
+
+    }
 
   ngOnInit() {
-  }
+
+  } 
+
+  public rollDices(){
+    let dicesRandom: number[]=new Array(); 
+    for (let index = 0; index < 4; index++) {
+
+      dicesRandom.push(Math.floor(Math.random()*3));
+
+    }
+    
+    this.dices=dicesRandom;
+ 
+ }
 
 }
